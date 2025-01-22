66f6aa367a9d5d134472d55b092fd9818def81ea
Sharikha Muskaan <134941525+SharikhaMuskaan@users.noreply.github.com>
2025-01-22 20:18:48 +0530

    Update browse-locustfile.py

--- browse-locustfile.py
+++ browse-locustfile.py
@@ -1,39 +1,50 @@
-from locust import task, run_single_user, FastHttpUser
+import json
+
+import products
+from cart import dao
+from products import Product
 
 
-class BrowseUser(FastHttpUser):
-    """
-    A user class simulating a user browsing the site.
-    """
-    host = "http://localhost:5000"
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
 
-    @task
-    def browse_page(self):
-        """
-        Task to simulate browsing the `/browse` endpoint.
-        """
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection": "keep-alive",
-            "DNT": "1",
-            "Host": "localhost:5000",
-            "Priority": "u=0, i",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "cross-site",
-            "Sec-GPC": "1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
-        }
-
-        with self.client.get("/browse", headers=headers, catch_response=True) as response:
-            if response.status_code == 200:
-                response.success()
-            else:
-                response.failure(f"Failed to browse page. Status code: {response.status_code}, Response: {response.text}")
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
 
 
-if __name__ == "__main__":
-    run_single_user(BrowseUser)
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if cart_details is None:
+        return []
+    
+    items = []
+    for cart_detail in cart_details:
+        contents = cart_detail['contents']
+        evaluated_contents = eval(contents)  
+        for content in evaluated_contents:
+            items.append(content)
+    
+    i2 = []
+    for i in items:
+        temp_product = products.get_product(i)
+        i2.append(temp_product)
+    return i2
+
+    
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
+
+
